58ab5bc79f7412582234c3aa6503eccb36140dbe
hashemthe1 <hashemthe1@gmail.com>
2023-09-19 16:20:58 +0300

    app init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+ 
+  imports: [RouterModule.forRoot(routes), FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toDoList';
+  // Array to store the Tasks
+  list:any[]=[]
+  // filter value keep track
+  filterStatus = 'all'; 
+  
+// method to add  Tasks
+  addTask(item: string){
+    const currentTime: Date = new Date();  
+
+    this.list.push({id:this.list.length, name:item ,completed: false,     timestamp: currentTime.toISOString()  
+    })
+
+
+    console.warn(this.list);
+  }
+ 
+// Remove Task Function
+  removeTask(selIndex:number){     
+     
+    this.list.splice(selIndex,1)  
+  }
+// Filter tasks based on the option selected
+  get filteredList() {
+    if (this.filterStatus === 'completed') {
+      return this.list.filter(item => item.completed);
+    } else {
+      return this.list;
+    }
+  }
 }
+
